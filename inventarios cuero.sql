ee41ef7a3c4ea99721a82514a33c70aedb57d82b
César Domingo Luna Gutiérrez <cdlunagtz@outlook.com>
2020-06-18 01:52:44 -0500

    Tablas y configuración de conexiones a BD

--- inventarios cuero.sql
+++ inventarios cuero.sql
@@ -4,117 +4,96 @@ use esmar;
 
 
 create table tipoProductoCuero(
-idTipoProducto int not null auto_increment primary key,
+idTipoProducto int not null primary key,
 tipoProducto varchar(30)
 ); 
 
 
 create table tipoCalibre(
-idTipoCalibre int not null auto_increment primary key,
+idTipoCalibre int not null primary key,
 descripcion varchar(30)
 );
 
 
 create table seleccionCuero(
-idSeleccion int not null auto_increment primary key,
+idSeleccion int not null primary key,
 seleccion varchar (30)
 );
 
 
 create table cueroTrabajar(
 idCueroTrabajar int not null auto_increment primary key,
-idTipoProducto int not null,
 noPartida int,
+tipoProducto varchar(20),
 noPiezas int,
 noPiezasActuales int,
 descripcion varchar(30),
-fecha date,
-key (idTipoProducto),
-foreign key (idTipoProducto) references tipoProductoCuero (idTipoProducto)
-on delete cascade on update cascade
+fecha date
 );
 
 
 create table calibrar(
 idCalibrar int not null auto_increment primary key,
-idCueroTrabajar int not null,
-idTipoCalibre int not null,
+noPartida int,
+tipoProducto varchar(20),
+calibre varchar(20),
 noPiezas int,
 noPiezasActuales int,
-fecha date,
-key (idCueroTrabajar),
-foreign key (idCueroTrabajar) references cueroTrabajar (idCueroTrabajar)
-on delete cascade on update cascade,
-key (idTipoCalibre),
-foreign key (idTipoCalibre) references tipoCalibre (idTipoCalibre)
-on delete cascade on update cascade
+descripcion varchar(30),
+fecha date
 );
 
 
 create table cueroSeleccionado(
 idcueroSeleccionado int not null auto_increment primary key,
-idCalibrar int not null,
-idSeleccion int not null,
+noPartida int,
+tipoProducto varchar(20),
+calibre varchar(20),
+seleccion varchar(20),
 noPiezas int,
 noPiezasActuales int,
-fecha date,
-key (idCalibrar),
-foreign key (idCalibrar) references calibrar (idCalibrar)
-on delete cascade on update cascade,
-key (idSeleccion),
-foreign key (idSeleccion) references seleccionCuero (idSeleccion)
-on delete cascade on update cascade
+descripcion varchar(30),
+fecha date
 );
 
 
 create table cueroPesado(
 idcueroPesado int not null auto_increment primary key,
-idcueroSeleccionado int not null,
+noPartida int,
+tipoProducto varchar(20),
+calibre varchar(20),
+seleccion varchar(20),
 peso float,
-pesoActual float,
 noPiezas int,
 noPiezasActuales int,
-fecha date,
-key (idcueroSeleccionado),
-foreign key (idcueroSeleccionado) references cueroSeleccionado (idcueroSeleccionado)
-on delete cascade on update cascade
+descripcion varchar(30),
+fecha date
 );
-
 
 create table invDispTrabajar(
 IdinvDispTrabajar int not null auto_increment primary key,
-idcueroPesado int not null,
+noPartida int,
+tipoProducto varchar(20),
+calibre varchar(20),
+seleccion varchar(30),
 peso float,
-pesoActual float,
 noPiezas int,
 noPiezasActuales int,
-fecha date,
-key (idcueroPesado),
-foreign key (idcueroPesado) references cueroPesado (idcueroPesado)
-on delete cascade on update cascade
+descripcion varchar(30),
+fecha date
 );
 
 
 create table productoSaldo(
 idProductoSaldo int not null auto_increment primary key,
-idTipoProducto int not null,
-idTipoCalibre int not null,
-idSeleccion int not null,
+tipoProducto varchar(20),
+calibre varchar(20),
+seleccion varchar(20),
 peso float,
-pesoActual float,
 noPiezas int,
 noPiezasActuales int,
 descripcion varchar(30),
-fecha date,
-key (idTipoProducto),
-foreign key (idTipoProducto) references tipoProductoCuero (idTipoProducto)
-on delete cascade on update cascade,
-key (idTipoCalibre),
-foreign key (idTipoCalibre) references tipoCalibre (idTipoCalibre)
-on delete cascade on update cascade,
-key (idSeleccion),
-foreign key (idSeleccion) references seleccionCuero (idSeleccion)
-on delete cascade on update cascade
+fecha date
 );
 
 
@@ -125,7 +104,6 @@ pesoActual float,
 descripcion varchar(30),
 fecha date
 );
-
 
 create table saldoTerminadoTrabajar(
 idSaldoTerminadoTrabajar int not null auto_increment primary key,
@@ -139,89 +117,6 @@ foreign key (idProductoSaldo) references productoSaldo (idProductoSaldo)
 on delete cascade on update cascade
 );
 
-
-create table pedaceraTrabajar(
-idPedacera int not null auto_increment primary key,
-idPedacerafk int not null,
-peso float,
-pesoActual float,
-fecha date,
-foreign key (idPedacerafk) references pedacera (idPedacera)
-on delete cascade on update cascade
-);
-
-
-create table terminadoVenta(
-idTerminadoVenta int not null auto_increment primary key,
-idInvDispTrabajar int not null,
-peso float,
-pesoActual float,
-noPiezas int,
-noPiezasActuales int,
-fecha date,
-key (idInvDispTrabajar),
-foreign key (idInvDispTrabajar) references invDispTrabajar (idInvDispTrabajar)
-on delete cascade on update cascade
-);
-
-
-create table saldoVenta(
-idSaldoVenta int not null auto_increment primary key,
-idSaldoTerminadoTrabajar int not null,
-peso float,
-pesoActual float,
-noPiezas int,
-noPiezasActuales int,
-fecha date,
-key (idSaldoTerminadoTrabajar),
-foreign key (idSaldoTerminadoTrabajar) references saldoTerminadoTrabajar (idSaldoTerminadoTrabajar)
-on delete cascade on update cascade
-);
-
-
-create table pedaceraVenta(
-idPedaceraVenta int not null primary key,
-idPedacera int not null,
-peso float,
-pesoActual float,
-fecha date,
-key (idPedacera),
-foreign key (idPedacera) references pedaceraTrabajar (idPedacera)
-on delete cascade on update cascade
-);
-
-
-create table cliente(
-idCliente int not null primary key,
-nombre varchar (40),
-calle varchar (40),
-colonia varchar (40),
-noInterior varchar (10),
-noExterior varchar (10),
-contacto varchar(40),
-rfc varchar()
-);
-
-
-create table pedido(
-);
-
-
-create table pedidoDetalle(
-);
-
-create table moneda(
-);
-
-
-create table venta(
-);
-
-
-create table ventaDetalle(
-);
-
-
 create table usuario(
 idUsuario int not null AUTO_INCREMENT  primary key,
 usuario varchar(15),
@@ -230,16 +125,56 @@ nombre varchar(30),
 tipo varchar(20)
 );
 
-
 insert into tipoProductoCuero values 
-(null,'Crupon Sillero'), (null,'Centro Quebracho'), (null,'Centro Castaño'), (null,'Delantero Sillero'), (null,'Delantero Doble'), (null,'Delantero Suela');
+(2,'Delantero Sillero'), (3,'Crupon Sillero'), (5,'Centro Castaño'), (6,'Centro Quebracho'), (7,'Delantero Suela'), (9,'Delantero Doble');
 
 insert into tipoCalibre values 
-(null,'4-5'), (null,'5-6'), (null,'6-7'), (null,'7-8'), (null,'8-9'), (null,'9-10'), (null,'10-11'), (null,'40-45'), (null,'45-50'), (null,'50-55'), (null,'55-60'), (null,'60-65'), (null,'65-70');
+(1,'4-5'),
+(2,'5-6'),
+(3,'6-7'),
+(4,'7-8'),
+(5,'8-9'),
+(6,'9-10'),
+(7,'10-11'),
+(8,'40-45'),
+(9,'45-50'),
+(10,'50-55'),
+(11,'55-60'),
+(12,'60-65'),
+(13,'65-70'),
+(14,'3-3.5'),
+(15,'3.5-4'),
+(16,'2.5-3'),
+(17,'3.2-3.6'),
+(18,'2-2.8'),
+(19,'2.8-3.2'),
+(20,'POR CALIBRAR'),
+(21,'3.5-4.1');
 
-insert into seleccionCuero values 
-(null,'Estandar'), (null,'B'), (null,'C'), (null,'D'), (null,'E'), (null,'F');
+insert into seleccionCuero values
+(1,	'Estandar'),
+(2,	'A'),
+(3,	'B'),
+(4,	'C'),
+(5,	'D'),
+(6,	'E'),
+(7,	'F'),
+(8,	'TP'),
+(9, '1'),
+(10,'2'),
+(11,'3'),
+(12,'4'),
+(13,'5'),
+(14,'PARA CINTO'),
+(15,'ESTANDAR PULIDO'),
+(16,'SALDO AW'),
+(17,'C PULIDO'),
+(18,'PARA CERCO'),
+(19,'PULIDOS'),
+(20,'STD'),
+(21,'POR SELECCIONAR'),
+(22,'P/EXPORTACION');
 
 insert into usuario values 
-(null,'mingo','123','Domingo Luna','Sistemas'), (null,'manuel','463mr','Manuel Rodriguez','Semiterminado'), (null,'fernando','f.robless','Fernando Robles','Produccion');
+(null,'sis','pa55word','Domingo Luna','Sistemas'), (null,'siss','pa55word','Domingo Luna','Semiterminado'), (null,'adolfo','adayala16','Adolfo Ayala','Semiterminado'), (null,'fernando','f.robless','Fernando Robles','Produccion');
 
